34da3f87955752340b637a2e55752a2ac5c48b22
Simon Pettersson <simon.v.pettersson@gmail.com>
2017-11-29 23:50:07 +0100

    Moved copying operation to correct place

--- test/redux.cpp
+++ test/redux.cpp
@@ -9,28 +9,48 @@
 #include <vector>
 
 struct A {
-  constexpr A(int v = 0) : value{v} {}
-  int value;
+  int value = 0;
+  int copied = 0;
+  A(int v) : value{v} {}
+  A(A &&other)
+      : value{std::move(other.value)}, copied{std::move(other.copied)} {}
+  A(const A &other) {
+    value = other.value;
+    copied = other.copied + 1;
+  }
 };
+
 struct B {
-  constexpr B(int v = 0) : value{v} {}
-  int value;
+  int value = 0;
+  int copied = 0;
+  B(int v) : value{v} {}
+  B(B &&other)
+      : value{std::move(other.value)}, copied{std::move(other.copied)} {}
+  B(const B &other) {
+    value = other.value;
+    copied = other.copied + 1;
+  }
 };
 
 namespace redux::detail {
 template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template <class... Ts> overloaded(Ts...)->overloaded<Ts...>;
 
-template <typename... Ts>
-constexpr auto unpack(const std::tuple<Ts...> &value) {
+template <typename... Ts> constexpr auto unpack(std::tuple<Ts...> &value) {
   return std::apply(
-      [](auto &... x) { return std::array{std::variant<Ts...>{x}...}; }, value);
+      [](auto &... x) {
+        return std::array{std::variant<Ts...>{std::move(x)}...};
+      },
+      value);
 }
 
 template <typename... Ts, std::size_t N>
-constexpr auto pack(const std::array<std::variant<Ts...>, N> &arr) {
+constexpr auto pack(std::array<std::variant<Ts...>, N> &arr) {
   return std::apply(
-      [](auto &... x) { return std::tuple<Ts...>{std::get<Ts>(x)...}; }, arr);
+      [](auto &... x) {
+        return std::tuple<Ts...>{std::get<Ts>(std::move(x))...};
+      },
+      arr);
 }
 } // namespace redux::detail
 
@@ -49,21 +69,16 @@ auto combine_reducers(auto &&... fs) {
 
 SCENARIO("Combine Reducers") {
   struct count_up {
-    constexpr count_up(int v) : value{v} {}
-    int value;
+    const int value = 0;
   };
 
   struct count_down {
-    constexpr count_down(int v) : value{v} {}
-    int value;
+    const int value = 0;
   };
 
   struct multiply {
-    constexpr multiply(int v) : value{v} {}
-    int value;
+    const int value = 0;
   };
-
-  constexpr auto cstate = std::make_tuple<A, B>(A{0}, B{5});
 
   auto reduce = redux::combine_reducers(
       [](A &state, const count_up &action) { state.value += action.value; },
@@ -72,34 +87,27 @@ SCENARIO("Combine Reducers") {
       [](auto &, const auto &) {});
 
   using V = std::variant<count_up, count_down, multiply>;
+  auto s0 = std::make_tuple<A, B>(A{0}, B{5});
+  CHECK(std::get<A>(s0).copied == 0);
+  CHECK(std::get<B>(s0).copied == 0);
 
-  auto s1 = reduce(cstate, V{count_up{3}});
+  auto s1 = reduce(s0, V{count_up{3}});
   CHECK(std::get<A>(s1).value == 3);
+  CHECK(std::get<A>(s1).copied == 1);
+  CHECK(std::get<B>(s1).copied == 1);
 
   auto s2 = reduce(s1, V{count_up{3}});
   CHECK(std::get<A>(s2).value == 6);
+  CHECK(std::get<A>(s2).copied == 2);
+  CHECK(std::get<B>(s2).copied == 2);
 
   auto s3 = reduce(s2, V{count_down{1}});
   CHECK(std::get<A>(s3).value == 5);
+  CHECK(std::get<A>(s3).copied == 3);
+  CHECK(std::get<B>(s3).copied == 3);
 
   auto s4 = reduce(s3, V{multiply{2}});
   CHECK(std::get<B>(s4).value == 10);
-}
-
-SCENARIO("Pack") {
-
-  using V = std::variant<A, B>;
-  constexpr auto src = std::array{V{A{1}}, V{B{2}}};
-  constexpr auto dst = redux::detail::pack(src);
-
-  CHECK(std::get<0>(dst).value == 1);
-  CHECK(std::get<1>(dst).value == 2);
-}
-
-SCENARIO("Unpack") {
-  constexpr auto src = std::make_tuple<A, B>(A{1}, B{2});
-  constexpr auto dst = redux::detail::unpack(src);
-
-  CHECK(std::get<A>(dst[0]).value == 1);
-  CHECK(std::get<B>(dst[1]).value == 2);
+  CHECK(std::get<A>(s4).copied == 4);
+  CHECK(std::get<B>(s4).copied == 4);
 }
