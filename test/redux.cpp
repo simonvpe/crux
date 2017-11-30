042ecee699e8b6216b82ab7552ec803340b53c59
Simon Pettersson <simon.v.pettersson@gmail.com>
2017-11-30 22:14:55 +0100

    EUREKA

--- test/redux.cpp
+++ test/redux.cpp
@@ -2,18 +2,15 @@
 #include <doctest.h>
 #include <redux.hpp>
 
-#include <array>
-#include <functional>
 #include <tuple>
+#include <type_traits>
 #include <variant>
-#include <vector>
 
 struct A {
   int value = 0;
   int copied = 0;
   A(int v) : value{v} {}
-  A(A &&other)
-      : value{std::move(other.value)}, copied{std::move(other.copied)} {}
+  A(A &&) = default;
   A(const A &other) {
     value = other.value;
     copied = other.copied + 1;
@@ -23,14 +20,14 @@ struct A {
     copied = other.copied + 1;
     return *this;
   }
+  A &operator=(A &&) = default;
 };
 
 struct B {
   int value = 0;
   int copied = 0;
   B(int v) : value{v} {}
-  B(B &&other)
-      : value{std::move(other.value)}, copied{std::move(other.copied)} {}
+  B(B &&) = default;
   B(const B &other) {
     value = other.value;
     copied = other.copied + 1;
@@ -38,58 +35,90 @@ struct B {
   B &operator=(const B &other) {
     value = other.value;
     copied = other.copied + 1;
+    if (copied == 2)
+      throw "Too much";
     return *this;
   }
+  B &operator=(B &&) = default;
+};
+
+struct count_up {
+  const int value = 0;
+};
+
+struct count_down {
+  const int value = 0;
+};
+
+struct multiply {
+  const int value = 0;
 };
 
 namespace redux::detail {
 template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template <class... Ts> overloaded(Ts...)->overloaded<Ts...>;
+template <typename T, typename... Ts> constexpr bool contains() {
+  return std::disjunction_v<std::is_same<T, Ts>...>;
+}
 } // namespace redux::detail
 
 namespace redux {
 auto combine_reducers(auto &&... fs) {
-  const auto dispatch = detail::overloaded{fs...};
+  const auto dispatch =
+      detail::overloaded{fs..., [](auto &&s, auto &&) { return s; }};
   return [&](const auto &state, const auto &action) {
-    const auto f = [&](const auto &s) {
-      return std::move(dispatch(s, action));
-    };
+    const auto f = [&](const auto &s) { return dispatch(s, action); };
     const auto g = [&](const auto &... s) { return std::make_tuple(f(s)...); };
     return std::apply(g, state);
   };
 }
+
+template <typename TReducer, typename... TStates>
+struct store : std::tuple<TStates...> {
+  using T = std::tuple<TStates...>;
+
+  constexpr store(TReducer reducer, TStates &&... t)
+      : std::tuple<TStates...>{std::forward<TStates>(t)...}, reduce{reducer} {}
+
+  const T &state() const { return *static_cast<const T *>(this); }
+
+  void dispatch(const auto &action) {
+    const auto copies = std::get<A>(*this).copied;
+    *static_cast<T *>(this) = reduce(state(), action);
+    CHECK(copies + 1 == std::get<A>(*this).copied);
+  }
+
+  const TReducer reduce;
+};
 } // namespace redux
 
+const auto reduce = redux::combine_reducers(
+    [](const A &state, const count_up &action) {
+      auto next_state = state;
+      next_state.value += action.value;
+      return next_state;
+    },
+    [](const A &state, const count_down &action) {
+      auto next_state = state;
+      next_state.value -= action.value;
+      return next_state;
+    },
+    [](const B &state, const multiply &action) {
+      auto next_state = state;
+      next_state.value *= action.value;
+      return next_state;
+    });
+
+SCENARIO("Store") {
+  auto store = redux::store{reduce, A{0}, B{7}};
+  CHECK(std::get<A>(store.state()).value == 0);
+
+  store.dispatch(count_up{5});
+  CHECK(std::get<A>(store.state()).value == 5);
+  CHECK(std::get<A>(store.state()).copied == 1);
+}
+
 SCENARIO("Reduce") {
-  struct count_up {
-    const int value = 0;
-  };
-
-  struct count_down {
-    const int value = 0;
-  };
-
-  struct multiply {
-    const int value = 0;
-  };
-
-  auto reduce = redux::combine_reducers(
-      [](const A &state, const count_up &action) {
-        auto next_state = state;
-        next_state.value += action.value;
-        return next_state;
-      },
-      [](const A &state, const count_down &action) {
-        auto next_state = state;
-        next_state.value -= action.value;
-        return next_state;
-      },
-      [](const B &state, const multiply &action) {
-        auto next_state = state;
-        next_state.value *= action.value;
-        return next_state;
-      },
-      [](const auto &state, const auto &) { return state; });
 
   auto s0 = std::make_tuple<A, B>(0, 5);
   CHECK(std::get<A>(s0).copied == 0);
